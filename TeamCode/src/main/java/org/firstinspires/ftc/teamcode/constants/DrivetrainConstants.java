abaa8a9d53a888c8a9dd3b4f22b91b122494afb8
AnthonyHoneine <anthonyhoneine@hotmail.com>
2024-01-25 15:09:10 -0500

    added drivetrain classes, AHRS cannot be resolved

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/constants/DrivetrainConstants.java
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.teamcode.constants;
+
+/**
+ * Constants for the Drivetrain
+ */
+public interface DrivetrainConstants {
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Drivetrain.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode.subsystems;
+
+import org.firstinspires.ftc.teamcode.constants.DrivetrainConstants;
+
+/**
+ * Robot Drivetrain
+ */
+public class Drivetrain implements DrivetrainConstants {
+
+}
